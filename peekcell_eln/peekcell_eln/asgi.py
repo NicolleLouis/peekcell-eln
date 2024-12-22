8dade3daeb4c3a0cf113b21ecf7607269d0d9368
Louis Nicolle <louisxnicolle@gmail.com>
2024-12-22 11:45:02 +0100

    Scaffolding

---
+++ peekcell_eln/peekcell_eln/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for peekcell_eln project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peekcell_eln.settings')
+
+application = get_asgi_application()
